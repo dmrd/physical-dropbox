ab365979e36d22d281ad0794f94246cc6003dc6f
David Dohan <git@ddohan.com>
2013-08-05 15:57:27 -0700

    Initial planning

---
+++ src/processor.py
@@ -0,0 +1,11 @@
+class Processor:
+    def __init__(self):
+        pass
+
+    def process_picture(self, picture):
+        pass
+
+    def process_pictures(self, pictures):
+        for picture in pictures:
+            self.process_picture(picture)
+
---
+++ src/scanner.py
@@ -0,0 +1,34 @@
+class Turntable:
+    def __init__(self, serial_object):
+        pass
+
+    def step(self, angle):
+        pass
+
+
+class Laser:
+    def __init__(self, serial_object):
+        pass
+
+    def on(self):
+        pass
+
+    def off(self):
+        pass
+
+
+class Camera:
+    def __init__(self):
+        pass
+
+    def take_picture(self):
+        pass
+
+    def transfer_picture(self):
+        pass
+
+    def transfer_pictures(self):
+        pass
+
+    def set_exposure(self):
+        pass
