b2a88afb29c9831f19c9b5c0ac091b32b7898445
David Dohan <git@ddohan.com>
2013-08-06 12:12:54 -0700

    Begin processor logic

--- src/camera_test/threshold.py
+++ src/camera_test/threshold.py
@@ -4,7 +4,7 @@ import os
 
 def thresh(img_name):
     img = cv2.imread(img_name, cv2.CV_LOAD_IMAGE_GRAYSCALE)
-    
+
     # sort bar by order of values in foo
     flatrank = np.argsort(img.ravel())
     # get the top n% of pixels
@@ -12,7 +12,7 @@ def thresh(img_name):
     thresh_index = flatrank[int(len(flatrank) * (1 - 0.01*n))]
     thresh_value = np.ravel(img)[thresh_index]
     print thresh_value
-   
+
     img = cv2.threshold(img, thresh_value, 255, cv2.THRESH_BINARY)[1]
     return img
 
@@ -20,5 +20,5 @@ if __name__=="__main__":
     img_name = 'Picture 4.jpg'
     img = thresh(img_name)
     cv2.imshow("%s_thresh.png" % os.path.splitext(img_name)[0], img)
-    cv2.waitKey(0)
+    #cv2.waitKey(0)
 
--- src/processor.py
+++ src/processor.py
@@ -1,10 +1,57 @@
+import cv2
+import numpy as np
+
+def thresh(color_img):
+    ''' Threshold the image so that the most intense pixels are white '''
+    bw_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
+
+    # sort bar by order of values in foo
+    flatrank = np.argsort(bw_img.ravel())
+    # get the top n% of pixels
+    n = 0.35
+    thresh_index = flatrank[int(len(flatrank) * (1 - 0.01*n))]
+    thresh_value = np.ravel(bw_img)[thresh_index]
+
+    bw_img = cv2.threshold(bw_img, thresh_value, 255, cv2.THRESH_BINARY)[1]
+    return bw_img
+
+
+def line_coords(thresholded):
+    '''
+    Return [a list of (x,y)] tuples representing the middle white pixel of each
+        line for which one exists
+    '''
+    pixels = []
+    for y, line in enumerate(thresholded):
+        white_pix = []
+        for x,pixel in enumerate(line):
+            if pixel == 0:
+                white_pix.append(x)
+        if white_pix:
+            # Add x,y tuple
+            pixels.append((white_pix[int(len(white_pix) / 2)], y))
+    return pixels
+
+
+def process_line(line_coords, angle):
+    '''
+    Return [a list of (x,y,z)] tuples representing the point cloud calculated
+        from line_coords and the given angle
+    '''
+    pass
+
+
 class Processor:
     def __init__(self):
-        pass
+        self.point_cloud = []
+
 
     def process_picture(self, picture, angle):
-        ''' Takes picture and angle (in radians).  Adds to point cloud '''
-        pass
+        ''' Takes picture and angle (in degrees).  Adds to point cloud '''
+        thresholded = thresh(picture)                 # Do a hard threshold of the image
+        pixels = line_coords(thresholded)             # Get line coords from image
+        self.point_cloud.extend(process_line(pixels)) # Add new points to cloud
+
 
     def process_pictures(self, pictures):
         for picture in pictures:
